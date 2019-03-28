76928bea3d06ce4b6632bfbfabd4b91da0a5aa17
Rav Smy <RavSmy@users.noreply.github.com>
2019-03-27 20:07:20 -0700

    Create 542_hw8_smyth.cpp

---
+++ 542_hw8_smyth.cpp
@@ -0,0 +1,72 @@
+//  542_hw8_smyth.cpp
+//  CS 542 - Discrete Structures
+//  Homework #8
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+stack <char> operators;
+stack <int> operands; 
+char priority[] = {'!', '&', '^', '|', '(' , ')'};
+
+bool evaluate(string exp);
+void operation();
+bool higherPriority(char a, char b);
+int find(char c);
+
+int main()
+{
+	string exp;
+
+	cout << "Input boolean expression: ";
+	cin >> exp; 
+
+	evaluate(exp); 
+
+}
+
+
+bool evaluate(string exp) {
+	for (int i = 0; i < exp.length; i++)
+		switch (exp[i]) {
+			case '0': operands.push(0); break;
+			case '1': operands.push(1); break;
+			case '(': operators.push('('); break;
+			case ')':
+				while (!operators.empty() && operators.top()!= '(')
+					operation();
+				if (!operators.empty()) operators.pop(); 
+				break;
+			default: 
+				if (operators.empty()) operands.push(exp[i]);
+				else while (!operators.empty() && !higherPriority(exp[i], operators.top()))
+					operation();
+				operators.push(exp[i]);
+		}
+}
+
+void operation() {
+	int a = operands.top(); operands.pop();
+	int b = operands.top(); operands.pop(); 
+	int op = operators.top(); operators.pop(); 
+
+	switch (op) {
+		case '!': operands.push(a);  operands.push(!b); break;
+		case '&': operands.push(a & b); break;
+		case '^': operands.push(a ^ b); break;
+		case '|': operands.push(a | b); break;
+	}
+}
+
+bool higherPriority(char a, char b) {
+	if (find(a) <= find(b)) return true;
+	return false; 
+}
+
+int find(char c) {
+	for (int i = 0; i < 4; i++)
+		if (c == priority[i]) return i;
+	return -1; 
+}
