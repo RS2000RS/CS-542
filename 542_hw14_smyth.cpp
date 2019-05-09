f3aaa09cbeaaa03d78364c3402e4da896390c3f8
Rav Smy <RavSmy@users.noreply.github.com>
2019-05-08 20:01:37 -0700

    stricly monotonic

--- 542_hw14_smyth.cpp
+++ 542_hw14_smyth.cpp
@@ -10,12 +10,15 @@ using namespace std;
 #define trials 10000
 
 double monotonic(unsigned a, unsigned b);
+double strictlyMonotonic(unsigned a, unsigned b);
 bool monoCheck(string a);
+bool strictMonoCheck(string a);
+
 
 int main()
 {
 
-	cout << monotonic(2, 3);
+	cout << strictlyMonotonic(2, 3);
 }
 
 double monotonic(unsigned a, unsigned b) {
@@ -28,6 +31,20 @@ double monotonic(unsigned a, unsigned b) {
 		for (int i = 0; i < b; i++) 
 			holder += to_string(rand() % a);
 		if (monoCheck(holder)) count++;
+	}
+
+	return double(count) / trials;
+}
+
+double strictlyMonotonic(unsigned a, unsigned b) {
+	int count = 0;
+	string holder;
+
+	for (int j = 0; j < trials; j++) {
+		holder = "";
+		for (int i = 0; i < b; i++)
+			holder += to_string(rand() % a);
+		if (strictMonoCheck(holder)) count++;
 	}
 
 	return double(count) / trials;
@@ -47,3 +64,21 @@ bool monoCheck(string a) {
 	}
 	return true;
 }
+
+bool strictMonoCheck(string a) {
+	char holder = a[0];
+	int direction = -2;
+
+	for (char c : a) {
+		if (direction == -2) direction++; // awfully written
+		else if (direction == -1) {
+			if (c == holder) return false;
+			else if (c > holder) direction = 0;
+			else if (c < holder) direction = 1;
+		}
+		else if (c == holder || (c > holder && direction == 1) || (c < holder && direction == 0)) return false;
+		holder = c;
+	}
+	return true;
+}
+
